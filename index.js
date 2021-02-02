daef3b793cb7f2a87892581a35758e1d84daef63
snkzt <snkzt@mercari.com>
2021-02-02 20:29:47 +0900

    modification on index.js

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'assets')))
-  .set('assets', path.join(__dirname, 'assets'))
-  .set('view engine', 'html')
   .get('/', (req, res) => res.render('HTML/home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
