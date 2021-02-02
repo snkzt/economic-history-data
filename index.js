eeeebbea72cb7413059e89713b9bdb16e16ab786
snkzt <snkzt@mercari.com>
2021-02-02 22:21:03 +0900

    modification in index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const { dirname } = require('path')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'assets')))
-  .get('/', (req, res) => res.render('HTML/home'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.use(express.static(path.join(__dirname, 'assets')))
+  app.get('/', (req, res) => {
+    console.log(req)
+    res.sendFile(path.join(__dirname,'assets/HTML/home.html'))  
+  })
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
