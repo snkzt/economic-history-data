014ee5b45d895f4da13ae76844f2fb44cc14627d
snkzt <snkzt@mercari.com>
2021-02-02 20:56:14 +0900

    modified index.js

--- index.js
+++ index.js
@@ -3,8 +3,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'assets')))
-  .set('views', path.join(__dirname, 'assets'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('HTML/home'))
+  .get('/', (req, res) => res.render('assets/HTML/home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
