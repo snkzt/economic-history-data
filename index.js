62bc6cdc0298ce265af10558492fad2272dab906
snkzt <snkzt@mercari.com>
2021-02-02 21:26:52 +0900

    reorganised the contents of the app

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'assets')))
-  .set('views', path.join(__dirname, 'assets'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('HTML/home.html'))
+  .get('/', (req, res) => res.render('HTML/home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
