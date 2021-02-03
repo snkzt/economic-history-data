dcdb35dc30e73d2c66c2a005ac4a877462b78d2a
snkzt <snkzt@mercari.com>
2021-02-03 23:21:55 +0900

    modified index.js for deploy

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'assets')))
-app.use(express.static('assets'))
-app.get('/', (req, res) => res.sendFile(path.join(__dirname,'assets/HTML/home.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'assets/html/home.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'assets/css/home.css')))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
