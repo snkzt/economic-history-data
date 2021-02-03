8a50445bdac90334b7b55c5047e1bb7e97b15039
snkzt <snkzt@mercari.com>
2021-02-03 21:45:33 +0900

    added a line to deploy CSS

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const economicHistoryData = require('economic-history-data')
 const express = require('express')
 const { dirname } = require('path')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
 
- // app.use(express.static(path.join(__dirname, 'assets')))
-  app.use(express.static('assets'))
-  app.get('/', (req, res) => res.sendFile(path.join(__dirname,'assets/HTML/home.html'))  )
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'assets')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'assets/HTML/home.html')))
+app.get('/cool', (req, res) => res.send(economicHistoryData()))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
