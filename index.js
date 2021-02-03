b6105d0f00bb34100647fade7e96954248d876aa
snkzt <snkzt@mercari.com>
2021-02-03 21:33:42 +0900

    added a line to deploy CSS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
 
-  app.use(express.static(path.join(__dirname, 'assets')))
+ // app.use(express.static(path.join(__dirname, 'assets')))
   app.use(express.static('assets'))
   app.get('/', (req, res) => res.sendFile(path.join(__dirname,'assets/HTML/home.html'))  )
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
