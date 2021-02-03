38cd996b8e7d143ac4e026bfd3affda3334ea1c5
snkzt <snkzt@mercari.com>
2021-02-03 23:11:09 +0900

    modified index.js for deploy whole codes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-//const { dirname } = require('path')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'assets')))
+app.use(express.static('assets'))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'assets/HTML/home.html')))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
